d206a300ce812f5fc84102cfa721be85ce7d7d21
Sajal Samaiya <sajal.samaiya.30@gmail.com>
2025-11-24 08:18:17 +0530

    Data ingestion and Transformation

---
+++ src/utils.py
@@ -0,0 +1,16 @@
+import os
+import sys
+from src.exception import CustomException
+
+import dill
+
+def save_object(file_path, obj):
+    try:# Implementation for saving an object to a file
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True) 
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+    
+    except Exception as e:
+        raise CustomException(e, sys)
+    
