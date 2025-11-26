42633b264ff539ef2f6f2d72703ee31c52fd0a22
Sajal Samaiya <sajal.samaiya.30@gmail.com>
2025-11-26 07:47:02 +0530

    model training

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,10 @@
 import os
 import sys
 from src.exception import CustomException
+from src.logger import logging
 
 import dill
+from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
     try:# Implementation for saving an object to a file
@@ -12,5 +14,21 @@ def save_object(file_path, obj):
             dill.dump(obj, file_obj)
     
     except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)
+            y_train_predicted_values = model.predict(X_train)
+            y_test_predicted_values = model.predict(X_test)
+            train_model_score = r2_score(y_train, y_train_predicted_values)
+            test_model_score = r2_score(y_test, y_test_predicted_values)
+            report[list(models.keys())[i]] = test_model_score
+        return report        
+    except Exception as e:
+        logging.info("Exception occurred during model evaluation")
         raise CustomException(e, sys)
     
